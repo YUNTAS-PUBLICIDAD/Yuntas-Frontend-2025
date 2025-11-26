2d0f910c1966357a08fb59f01efb31977fea6f71
Alexander <villafuertequispealex@gmail.com>
2025-11-26 17:10:08 -0500

    Producto detalle: responsive.

---
+++ src/components/organisms/productos/detalle/CotizaSection.tsx
@@ -0,0 +1,16 @@
+import Banner from "@/components/atoms/Banner"
+import Text from "@/components/atoms/Text"
+import Button from "@/components/atoms/Button";
+export default function CotizaSection() {
+	return (
+		<section className="w-full">
+			<Banner color='bg-[#E2F6F6]' className='flex flex-col h-auto gap-10 py-16 font-normal text-2xl '>
+				<Text variant="h2" color="text-[#203565]" className="font-bold">¿ENCONTRASTE LO QUE BUSCABAS?</Text>
+				<Button variant="tertiary" size="lg">
+					¡COTIZA AHORA!
+				</Button>
+
+			</Banner>
+		</section>
+	)
+}
