880a71c8af274d7e14b63fe960b3e6ad02e1a591
Alexander <villafuertequispealex@gmail.com>
2025-11-26 13:34:48 -0500

    Producto detalle: hero y especificacion completo

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -1,8 +1,35 @@
+'use client';
+
+import HeroSection from "@/components/organisms/productos/detalle/HeroSection";
+import heroBackground from "@/assets/productos/detalle/productoDetalle.jpg";
+import EspecificacionesSection from "@/components/organisms/productos/detalle/EspecificacionesSection";
+import especificacionProducto from "@/assets/productos/detalle/especificacionProducto.jpg";
+
+import { useParams } from "next/navigation";
+
+const productData = {
+  name: "LETREROS NEÓN LED",
+  heroImage: heroBackground.src,
+  specImage: especificacionProducto.src,
+  specs: [
+    "Iluminación LED de bajo consumo.",
+    "Amplia gama de colores personalizados.",
+    "Material PVC flexible y resistente."
+  ]
+};
+
 export default function ProductoDetallePage() {
+  const params = useParams<{ id: string }>();
+  console.log("id", params.id);
+
+  const product = productData;
+
   return (
-    <main className="p-8">
-      <h2 className="text-2xl font-semibold mb-4">Detalle del Producto</h2>
-      {/* Aquí irá la información del producto */}
+    <main>
+      <HeroSection productName={product.name} backgroundImage={product.heroImage} />
+      <EspecificacionesSection
+        specs={product.specs}
+        imageSrc={product.specImage} />
     </main>
   );
 }
