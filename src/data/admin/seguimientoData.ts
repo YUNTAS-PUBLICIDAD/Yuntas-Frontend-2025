9b9f837e55e97b367807b7a07622739b1146f870
Alexander <villafuertequispealex@gmail.com>
2025-11-28 16:41:14 -0500

    Datos de prueba para la seccion de seguimiento

---
+++ src/data/admin/seguimientoData.ts
@@ -0,0 +1,29 @@
+type DataSeguimientoItem = {
+    id: number;
+    nombre: string;
+    gmail: string;
+    telefono: string;
+    producto: string;
+    fecha: string;
+};
+
+const data: DataSeguimientoItem[] = [
+    {
+        id: 1,
+        nombre: "Kiara",
+        gmail: "namelose@gmail.com",
+        telefono: "941825478",
+        producto: "Mesas y sillas LED",
+        fecha: "25/01/2025"
+    },
+    {
+        id: 2,
+        nombre: "Maria",
+        gmail: "maria@gmail.com",
+        telefono: "999777666",
+        producto: "Pantallas LED",
+        fecha: "27/11/2025"
+    }
+];
+
+export default data;
