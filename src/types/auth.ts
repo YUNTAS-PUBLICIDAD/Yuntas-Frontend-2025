3397e5c312c610765c1888530b3363ff8036f297
Alexander <villafuertequispealex@gmail.com>
2025-12-02 15:02:41 -0500

    Tipado para el servicio de autenticacion

---
+++ src/types/auth.ts
@@ -0,0 +1,27 @@
+export interface LoginCredentials {
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    success: boolean;
+    message: string;
+    data: {
+        token: string;
+        user: {
+            id: number;
+            name: string;
+            email: string;
+            email_verified_at: null | string;
+            celular: string;
+            created_at: string;
+            updated_at: string;
+        }
+    };
+}
+
+export interface AuthError {
+    success: boolean;
+    message: string;
+    errors: null | number;
+}
