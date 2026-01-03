2cecc9d5a68108d7d23ed0ea51a9d91abfa41320
darkMorgan <fabriziozevallos159@gmail.com>
2026-01-03 16:50:57 -0500

    cambios config

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,34 @@
-/** @type {import('next').NextConfig} */
+/**@type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '8000',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'apiyuntas.yuntaspublicidad.com',
-            }
-        ],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  
+   
+/** holaaaa */
+
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/',
+      },
+      {
+        protocol: 'https',
+        hostname: 'apiyuntas.yuntaspublicidad.com',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
