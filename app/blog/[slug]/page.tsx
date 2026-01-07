1a71181a87e113bcff084feecac8b8502042a7b3
darkMorgan <fabriziozevallos159@gmail.com>
2026-01-07 13:52:46 -0500

    cambios en blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,7 +5,65 @@ import OpinionSection from "@/components/organisms/blog/blogId/OpinionSection";
 import VideoSection from "@/components/organisms/blog/blogId/VideoSection";
 import { blogStaticData } from "@/data/blogStaticData";
 
-export function generateStaticParams() {
+import { BlogView } from "@/types/admin/blog";
+
+async function getBlogs(): Promise<BlogView[]> {
+  const res = await fetch(
+    "https://apiyuntas.yuntaspublicidad.com/api/blogs",
+    { cache: "force-cache" }
+  );
+
+  const json = await res.json();
+  return json.data.data;
+}
+
+async function getBlogBySlug(slug: string): Promise<BlogView | null> {
+  const res = await fetch(
+    `https://apiyuntas.yuntaspublicidad.com/api/blogs/${slug}`,
+    { cache: "force-cache" }
+  );
+
+  if (!res.ok) {
+    return null;
+  }
+
+  const json = await res.json();
+  return json.data;
+}
+
+export async function generateStaticParams() {
+  const blogs = await getBlogs();
+  return blogs.map(blog => ({
+    slug: blog.slug
+  }));
+}
+export default async function BlogDetallePage({
+  params,
+}: { params: { slug: string} ;
+}) {
+  const blog =  await getBlogBySlug(params.slug);
+  
+  if (!blog) {
+    return <div>Post no encontrado</div>;
+}
+
+return (
+    <main>
+      <HeroPage
+      url={blog.main_image?.url}
+      text={blog.title}
+      position="medio"
+      />
+      <DescripcionSection data={blog} />
+      <BeneficiosSection data={blog} />
+      <OpinionSection data={blog} />
+      {blog.video_url && <VideoSection data={blog} />}
+    </main>
+  );
+}
+
+
+/* export function generateStaticParams() {
   return blogStaticData.map(post => ({
     slug: post.slug
   }));
@@ -30,3 +88,6 @@ export default function BlogDetallePage({ params }: { params: { slug: string } }
     </main>
   );
 }
+
+*/
+
