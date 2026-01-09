e3f6315fb7f95c68154378be77d3f632e159453e
yudi-star <yudith.pacco@tecsup.edu.pe>
2026-01-09 12:14:39 -0500

    Corrección error creación usuarios - Solución persistencia de Token

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -8,30 +8,32 @@ export async function loginService(credentials: LoginCredentials): Promise<Login
         const response = await api.post(API_ENDPOINTS.AUTH.LOGIN, credentials);
 
         const data = response.data;
-        const token = data.data?.token || data.token;
+        
+       
+        const token = data.data?.token || data.token || data.access_token;
         const user = data.data?.user || data.user;
 
         if (!token) {
             return { success: false, message: "Error: No se recibió token del servidor." };
         }
 
-        // Guardar el token en cookies
         setToken(token);
+
+        localStorage.setItem("token", token);
 
         return {
             success: true,
             message: "Bienvenido",
             token: token,
             user: {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-
+                id: user?.id,
+                name: user?.name,
+                email: user?.email,
             },
-
         };
 
     } catch (error: any) {
+        console.error("Error en Login:", error); 
         if (error instanceof AxiosError) {
             const errorMessage = error.response?.data?.message || "Credenciales incorrectas";
             return { success: false, message: errorMessage };
@@ -40,9 +42,11 @@ export async function loginService(credentials: LoginCredentials): Promise<Login
         return { success: false, message: "Error de conexión con el servidor." };
     }
 }
+
 export async function logoutService(): Promise<LoginActionResponse> {
     try {
-        const token = getToken();
+        
+        const token = localStorage.getItem("token") || getToken();
 
         if (token) {
             await api.post(API_ENDPOINTS.AUTH.LOGOUT, {}, {
@@ -54,7 +58,9 @@ export async function logoutService(): Promise<LoginActionResponse> {
     } catch (error) {
         console.error("Error al cerrar sesión en backend", error);
     } finally {
+       
         removeToken();
+        localStorage.removeItem("token"); 
 
         return { success: true, message: "Sesión cerrada" };
     }
