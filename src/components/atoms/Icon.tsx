c06ce9d5028055c97ff8215cdf9156ebaae7d393
DanielRCor <thedaniel0405@gmail.com>
2026-01-13 13:26:37 -0500

    hero 13/01/26

--- src/components/atoms/Icon.tsx
+++ src/components/atoms/Icon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface IconProps {
-  children: React.ReactNode; // SVG o icono
+  children: React.ReactNode;
   size?: "sm" | "md" | "lg" | "xl" | "2xl" | number;
   bgColor?: string;
   className?: string;
@@ -14,23 +14,36 @@ const sizeMap = {
   md: "h-8 w-8",
   lg: "h-12 w-12",
   xl: "h-16 w-16",
-  "2xl": "h-24 w-24"
+  "2xl": "h-24 w-24",
 };
 
-const Icon: React.FC<IconProps> = ({ children, size = "md", bgColor = "bg-blue-900", className = "", href, label }) => {
-  const sizeClass = typeof size === "string" ? sizeMap[size] || "" : `h-[${size}px] w-[${size}px]`;
+const Icon: React.FC<IconProps> = ({
+  children,
+  size = "md",
+  bgColor = "bg-blue-900",
+  className = "",
+  href,
+  label,
+}) => {
+  const sizeClass =
+    typeof size === "string"
+      ? sizeMap[size] || ""
+      : `h-[${size}px] w-[${size}px]`;
+
   const content = (
     <div className={`${bgColor} rounded-full flex items-center justify-center p-2 ${sizeClass} ${className}`}>
       {children}
     </div>
   );
+
   if (href) {
     return (
-      <a href={href} aria-label={label} className="hover:text-blue-300">
+      <a href={href} aria-label={label}>
         {content}
       </a>
     );
   }
+
   return content;
 };
 
--- src/components/molecules/header/UserSection.tsx
+++ src/components/molecules/header/UserSection.tsx
@@ -1,11 +1,10 @@
 import Icon from "@/components/atoms/Icon";
 import Link from "next/link";
-import UserImg from "@/assets/inicio/user-1.png"; // <-- IMPORTA TU IMAGEN
+import UserImg from "@/assets/inicio/user-1.png";
 
 type UserSectionProps = {
   size?: "sm" | "md" | "lg";
   color?: string;
-  fill?: string;
 };
 
 export default function UserSection({
@@ -16,13 +15,13 @@ export default function UserSection({
     size === "sm" ? "scale-100" : size === "lg" ? "scale-125" : "scale-110";
 
   return (
-    <Link href={"/login"}>
+    <Link href="/login">
       <div className={`flex items-center gap-2 ${scaleClass}`}>
-        <Icon size={size} bgColor={color}>
+        <Icon size={size} bgColor={color} className="p-0">
           <img
             src={UserImg.src}
             alt="User Icon"
-            className="w-7 h-7 object-contain"
+            className="w-full h-full object-cover rounded-full"
           />
         </Icon>
       </div>
