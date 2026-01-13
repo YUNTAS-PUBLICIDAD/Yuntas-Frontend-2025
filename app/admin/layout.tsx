d50b3d7c274e99ecdde03b467d4970b2994350ce
AngieTembladera <mirtha.tembladera@tecsup.edu.pe>
2026-01-13 14:38:38 -0500

    conexión del dark mode con seguimiento del dashboard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,22 +4,48 @@ import SidebarSection from "@/components/organisms/admin/SidebarSection";
 import Encabezado from "@/components/molecules/Encabezado";
 import { useTokenValidation } from "@/hooks/useTokenValidation";
 import Loader from "@/components/atoms/Loader";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-    const { isLoading } = useTokenValidation();
+  const { isLoading } = useTokenValidation();
 
-    if (isLoading) {
-        return <Loader size="lg" />;
-    }
-
+  if (isLoading) {
     return (
-        <div className="flex flex-col">
-            <Encabezado variant="azul">SECCIÓN PRINCIPAL</Encabezado>
-            <div className="flex flex-1 bg-white"> {/*Color para el modo ocuro:  bg-[#203565] */}
-                <SidebarSection />
-                <main className="flex-1 py-16 px-8">
-                    {children}
-                </main>
-            </div>
-        </div>
+      <div className="
+        min-h-screen flex items-center justify-center
+        bg-white dark:bg-[#141A3F]
+      ">
+        <Loader size="lg" />
+      </div>
     );
+  }
+
+  return (
+    <div className="
+      flex flex-col min-h-screen
+      bg-white dark:bg-[#141A3F]
+      transition-colors duration-300
+    ">
+      {/* Encabezado */}
+      <Encabezado variant="azul">SECCIÓN PRINCIPAL</Encabezado>
+
+      {/* Contenido */}
+      <div className="
+        flex flex-1
+        bg-white dark:bg-[#141A3F]
+        transition-colors duration-300
+      ">
+        {/* Sidebar */}
+        <SidebarSection />
+
+        {/* Página */}
+        <main className="
+          flex-1 py-16 px-8
+          bg-white dark:bg-[#141A3F]
+          transition-colors duration-300
+        ">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
