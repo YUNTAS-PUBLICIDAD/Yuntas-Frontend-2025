54ae173788233988cc7ea13cb018a178b9cf19d8
DanielRCor <thedaniel0405@gmail.com>
2026-02-10 14:27:43 -0500

    reparado yuntas botones pagespeed

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -24,7 +24,7 @@ const Button: React.FC<ButtonProps> = ({
   const baseClasses = "font-bold rounded-2xl md:rounded-3xl transition-all duration-300 inline-block text-center disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:scale-100";
 
   const variantClasses = {
-    primary: "bg-[#23C1DE] text-[#04061a] hover:bg-[#5BC5C7]",
+    primary: "bg-[#23C1DE] text-white hover:bg-[#5BC5C7]",
     secondary: "bg-white text-[#0B0B1F] hover:bg-gray-100",
     tertiary: "bg-[#203565] text-white hover:bg-[#162E4D]",
     outline: "border-2 border-white text-white hover:bg-white hover:text-[#0B0B1F]",
