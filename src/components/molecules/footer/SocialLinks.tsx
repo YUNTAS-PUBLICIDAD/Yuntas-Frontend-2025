16757849bb8ef2fdf1d675421e98b7102fb8fb38
yudi-star <yudith.pacco@tecsup.edu.pe>
2026-02-16 11:14:52 -0500

    Ajuste en el icono de las redes sociales

--- src/components/molecules/footer/SocialLinks.tsx
+++ src/components/molecules/footer/SocialLinks.tsx
@@ -3,46 +3,49 @@ import Icon from "@/components/atoms/Icon";
 import { FaInstagram, FaFacebook, FaTiktok, FaYoutube } from "react-icons/fa";
 
 const SocialLinks = () => {
+    
+    const customSize = 40; 
+
     return (
         <div className="flex gap-4 justify-center md:justify-start">
             <Icon
                 href="https://www.instagram.com/yuntaspublicidad/"
                 target="_blank"
                 label="Instagram"
-                size="md"
+                size={customSize} 
                 bgColor="bg-transparent"
             >
-                <FaInstagram className="text-3xl text-white" />
+                <FaInstagram className="text-2xl text-white" />
             </Icon>
 
             <Icon
-                href="https://www.facebook.com/kp.yuntas.1/"
+                href="https://www.facebook.com/YuntasProducciones/"
                 target="_blank"
                 label="Facebook"
-                size="md"
+                size={customSize}
                 bgColor="bg-transparent"
             >
-                <FaFacebook className="text-3xl text-white" />
+                <FaFacebook className="text-2xl text-white" />
             </Icon>
 
             <Icon
                 href="https://www.tiktok.com/@yuntaspublicidad"
                 target="_blank"
                 label="TikTok"
-                size="md"
+                size={customSize}
                 bgColor="bg-transparent"
             >
-                <FaTiktok className="text-3xl text-white" />
+                <FaTiktok className="text-2xl text-white" />
             </Icon>
 
             <Icon
                 href="https://www.youtube.com/@yuntaspublicidad"
                 target="_blank"
                 label="YouTube"
-                size="md"
+                size={customSize}
                 bgColor="bg-transparent"
             >
-                <FaYoutube className="text-3xl text-white" />
+                <FaYoutube className="text-2xl text-white" />
             </Icon>
         </div>
     );
