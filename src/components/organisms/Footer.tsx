0f89fe4359a14c98e2d502b553d77e20c59a6e25
yudi-star <yudith.pacco@tecsup.edu.pe>
2026-02-17 11:25:25 -0500

    Rediseño del footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,30 +1,39 @@
 import LogoBlock from "@/components/molecules/footer/LogoBlock";
 import AddressBlock from "@/components/molecules/footer/AddressBlock";
+import LegalLinksBlock from "@/components/molecules/footer/LegalLinksBlock"; 
 import ContactBlock from "@/components/molecules/footer/ContactBlock";
 import SocialLinks from "@/components/molecules/footer/SocialLinks";
 
 export default function Footer() {
   return (
     <footer className="bg-[#04041C] text-white py-12 px-6">
-      <div className="max-w-7xl mx-auto flex flex-col items-center gap-12 md:flex-row md:items-start md:justify-center md:gap-x-24 lg:gap-x-40">
+      <div className="max-w-7xl mx-auto">
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 items-start justify-items-center md:justify-items-start">
 
-        {/* Logo and Mobile Header */}
-        <div className="flex flex-col items-center w-full md:w-auto gap-8">
-          <LogoBlock />
-          <div className="w-full h-[2px] bg-[#6DE1E3] md:hidden"></div>
-          <div className="md:hidden">
-            <SocialLinks />
+          <div className="flex flex-col items-center md:items-start w-full gap-8">
+            <LogoBlock />
+            <div className="w-full h-[2px] bg-[#6DE1E3] md:hidden"></div>
+            <div className="md:hidden">
+              <SocialLinks />
+            </div>
           </div>
-        </div>
 
-        {/* Dirección & Horario */}
-        <div className="w-full md:w-auto">
-          <AddressBlock />
-        </div>
+          <div className="w-full">
+            <LegalLinksBlock />
+          </div>
 
-        {/* Contacto & Desktop Social Links */}
-        <div className="w-full md:w-auto">
-          <ContactBlock />
+          {/* Dirección y Horario */}
+          <div className="w-full">
+            <AddressBlock />
+          </div>
+
+          {/*  Contacto y Redes (Desktop) */}
+          <div className="w-full flex flex-col gap-6">
+            <ContactBlock />
+         
+          </div>
+
         </div>
       </div>
     </footer>
