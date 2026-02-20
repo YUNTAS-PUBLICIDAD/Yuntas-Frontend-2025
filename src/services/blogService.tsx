8f63dd3ff4c788921f612b6351c599bde67659e4
Alexander <villafuertequispealex@gmail.com>
2026-02-20 11:49:55 -0500

    avance en service de blog

--- src/services/blogService.tsx
+++ src/services/blogService.tsx
@@ -1,24 +1,46 @@
 import { api, API_ENDPOINTS } from "@/config";
-import { Blog, BlogActionResponse, BlogListResponseBySlug } from "@/types/admin/blog";
+import { Blog, BlogServiceResponse, BlogInput } from "@/types/admin/blog";
 import { getToken } from "@/utils/token";
+import { getImg } from "@/utils/getImg";
 
-export async function getBlogsService(perPage: number = 10, url?: string): Promise<BlogActionResponse<Blog[]>> {
+function formatBlog(apiBlog: any): Blog {
+    return {
+        ...apiBlog,
+        main_image: {
+            url: typeof apiBlog.main_image.url === "string" ? `${getImg(apiBlog.main_image.url)}` : apiBlog.main_image.url,
+            alt: apiBlog.main_image.alt,
+            title: apiBlog.main_image.title,
+        },
+        gallery: apiBlog.gallery.map((img: any) => ({
+            slot: img.slot,
+            url: typeof img.url === "string" ? `${getImg(img.url)}` : img.url,
+            title: img.title,
+            alt: img.alt,
+        })),
+    };
+};
+
+export async function getBlogsService(perPage: number = 10, url?: string): Promise<BlogServiceResponse<Blog[]>> {
     try {
-        const endpoint = url ?? API_ENDPOINTS.BLOG.GET_ALL + `?per_page=${perPage}`;
-        const response = await api.get(endpoint);
+        const response = await api.get(API_ENDPOINTS.BLOG.GET_ALL, {
+            params: {
+                per_page: perPage,
+                url: url || undefined,
+            },
+        });
+
+        const formattedBlogs = response.data.data.data.map(formatBlog);
 
         return {
             success: true,
-            data: response.data.data,
-            meta: response.data.meta,
-            links: response.data.links,
+            data: formattedBlogs,
         };
     } catch (error: any) {
         return { success: false, message: error.message };
     }
 }
 
-export async function getBlogBySlugService(slug: string): Promise<BlogListResponseBySlug<Blog>> {
+export async function getBlogBySlugService(slug: string): Promise<BlogServiceResponse<Blog>> {
     try {
 
         const response = await api.get(API_ENDPOINTS.BLOG.GET_ONE(slug));
@@ -26,20 +48,20 @@ export async function getBlogBySlugService(slug: string): Promise<BlogListRespon
         return {
             success: true,
             message: response.data.message,
-            data: response.data.data,
+            data: formatBlog(response.data.data),
         };
     } catch (error: any) {
         return { success: false, message: error.message };
     }
 }
-
-export async function createBlogService(formData: FormData): Promise<BlogActionResponse<Blog>> {
+////////////////////////////////////// FALTA REFACTORIZAR ESTO  //////////////////////////////////////
+export async function createBlogService(formData: FormData): Promise<BlogServiceResponse<Blog>> {
     try {
         const token = getToken();
         if (!token) return { success: false, message: "No autenticado" };
 
         const response = await api.post(API_ENDPOINTS.ADMIN.BLOG.CREATE, formData, {
-            headers: { "Content-Type": "multipart/form-data" , Authorization: `Bearer ${token}` },
+            headers: { "Content-Type": "multipart/form-data", Authorization: `Bearer ${token}` },
         });
 
         return {
@@ -51,14 +73,14 @@ export async function createBlogService(formData: FormData): Promise<BlogActionR
         return { success: false, message: error.message };
     }
 }
-
-export async function updateBlogService(id: number | string, formData: FormData): Promise<BlogActionResponse<Blog>> {
+////////////////////////////////////// FALTA REFACTORIZAR ESTO  //////////////////////////////////////
+export async function updateBlogService(id: number | string, formData: FormData): Promise<BlogServiceResponse<Blog>> {
     try {
         const token = getToken();
         if (!token) return { success: false, message: "No autenticado" };
         formData.append("_method", "PUT");
         const response = await api.post(API_ENDPOINTS.ADMIN.BLOG.UPDATE(Number(id)), formData, {
-            headers: { "Content-Type": "multipart/form-data" , Authorization: `Bearer ${token}` },
+            headers: { "Content-Type": "multipart/form-data", Authorization: `Bearer ${token}` },
         });
         return {
             success: true,
@@ -70,13 +92,16 @@ export async function updateBlogService(id: number | string, formData: FormData)
     }
 }
 
-export async function deleteBlogService(id: number | string): Promise<BlogActionResponse<null>> {
+export async function deleteBlogService(id: number | string): Promise<BlogServiceResponse<null>> {
     try {
         const token = getToken();
+
         if (!token) return { success: false, message: "No autenticado" };
 
         await api.delete(API_ENDPOINTS.ADMIN.BLOG.DELETE(Number(id)), {
-            headers: { Authorization: `Bearer ${token}` },
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
         });
 
         return { success: true, message: "Blog eliminado exitosamente" };
