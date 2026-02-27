038af1666044fcdcfdf505041fcedf9863fd5b9b
yudi-star <yudith.pacco@tecsup.edu.pe>
2026-02-27 10:58:51 -0500

    Titulo centrado en detalles de blog

--- src/components/molecules/HeroPage.tsx
+++ src/components/molecules/HeroPage.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import Heading from '../atoms/Heading'
 import DividerLine from '../atoms/DividerLine';
@@ -20,23 +19,31 @@ type HeroPageProps = {
 const HeroPage = ({ url, text, position = "medio", imageTitle, imageAlt }: HeroPageProps) => {
   return (
     <section
-
-      className={`relative w-full h-screen flex items-center ${posicionesText[position]} 
-      overflow-hidden pt-24 bg-cover bg-center bg-no-repeat  px-8 md:px-0 text-center
-       md:h-screen h-[80vh] md:pt-24 `}
+      className={`relative w-full h-screen flex flex-col ${posicionesText[position]} 
+      overflow-hidden pt-24 bg-cover bg-center bg-no-repeat text-center
+      md:h-screen h-[80vh] md:pt-24`}
       style={{ backgroundImage: `url(${url})` }}
-
     >
       {imageAlt && <img src={url} alt={imageAlt} title={imageTitle} className="sr-only" aria-hidden="false" />}
+      
       <div className="absolute inset-0 bg-black/30 md:bg-black/40" />
-      <Heading
-        level="h1"
-        size="2xl"
-        className="leading-tight md:text-7xl text-5xl sm:text-6xl drop-shadow-[0_2px_2px_#28BEDA]"
-      >
-        {text}
-      </Heading>
-      <DividerLine />
+      
+      <div className="relative z-10 container mx-auto px-4 flex-grow flex flex-col justify-center items-center">
+        <div className="max-w-5xl">
+            <Heading
+                level="h1"
+                size="2xl"
+                className="leading-tight md:text-7xl text-5xl sm:text-6xl drop-shadow-[0_2px_2px_#28BEDA]"
+            >
+                {text}
+            </Heading>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full z-20">
+        <DividerLine />
+      </div>
+
     </section>
   );
 };
