df97ee6977f7d647dbcf3b81ead25fcf9a3e7d9d
diegovilcasaboya <diegovilcatut@gmail.com>
2026-03-13 10:40:38 -0500

    fix: arreglar menu hamburguesa

--- src/components/atoms/MenuItem.tsx
+++ src/components/atoms/MenuItem.tsx
@@ -2,16 +2,16 @@ interface MenuItemProps {
   href: string;
   children: React.ReactNode;
   active?: boolean;
-  color?:string,
+  color?:string;
+  className?: string;
 }
 
-export default function MenuItem({ href, children, active,color='text-gray-800' }: MenuItemProps) {
-  const defaultStyles = "text-gray-800 hover:text-blue-700 px-4 py-2";
-
-  const finalStyles = color ? color : defaultStyles;
-
-  return (
-   <a href={href} className={`transition ${active ? "text-blue-600 font-bold px-4 py-2" : finalStyles}`}>
+export default function MenuItem({ href, children, active, color = 'text-gray-800', className = "" }: MenuItemProps) {
+ return (
+    <a 
+      href={href} 
+      className={`block transition duration-300 ${active ? "text-blue-600 font-bold px-4 py-2" : `${color} px-4 py-2 hover:text-blue-700`} ${className}`}
+    >
       {children}
     </a>
   );
