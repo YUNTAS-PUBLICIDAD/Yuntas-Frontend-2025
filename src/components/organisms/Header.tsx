b440658fcd74854ee8bcb8e5b60fca277651f3e2
Bryan Mancilla <bmancilla119@gmail.com>
2026-04-10 10:53:55 -0500

    feat: mejorar layout en el header para un mejor responsive

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -9,17 +9,17 @@ export default function Header() {
   return (
     <>
       <header
-        className={`hidden md:flex sticky top-0 z-50 items-center px-16 py-5 h-24 
+        className={`hidden lg:flex sticky top-0 z-50 items-center justify-between px-16 py-5 h-24
           transition-all duration-300 bg-white shadow-md`}
       >
-        <div className="flex flex-col items-center w-56">
-         
-          <Link href="/" aria-label="Ir a la página de inicio">
+        {/*<div className="flex flex-col items-center shrink-0">*/}
+
+          <Link href="/" className="shrink-0" aria-label="Ir a la página de inicio">
             <Logo src="/logo.svg" size="xl" alt="Yuntas Publicidad" />
           </Link>
-        </div>
+        {/*</div>*/}
 
-        <div className="flex items-center gap-x-14 ml-auto">
+        <div className="flex items-center gap-x-6 xl:gap-x-10">
           <NavMenu size="lg" />
           <UserSection size="lg"  />
         </div>
