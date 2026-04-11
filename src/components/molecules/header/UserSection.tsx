22a5d51480bb0b0845404b77c397a104041a9f9f
Bryan Mancilla <bmancilla119@gmail.com>
2026-04-11 08:50:17 -0500

    fix: corregir scala por tamaño en el icono de user

--- src/components/molecules/header/UserSection.tsx
+++ src/components/molecules/header/UserSection.tsx
@@ -10,12 +10,22 @@ export default function UserSection({
   size = "md",
   enableDarkMode = false,
 }: UserSectionProps) {
-  const scaleClass =
-    size === "sm" ? "scale-100" : size === "lg" ? "scale-125" : "scale-110";
+  const sizeMap = {
+    sm: "w-8 h-8",
+    md: "w-10 h-10",
+    lg: "w-12 h-12"
+  };
+  const iconSizeMap = {
+    sm: "w-4 h-4",
+    md: "w-5 h-5",
+    lg: "w-6 h-6"
+  };
+  // const scaleClass =
+  //   size === "sm" ? "scale-100" : size === "lg" ? "scale-125" : "scale-110";
 
   return (
     <Link href="/login">
-      <div className={`flex items-center justify-center ${scaleClass}`}>
+      <div className={`flex items-center justify-center ${sizeMap[size]}`}>
         <Icon
           size={size}
           bgColor={
@@ -34,7 +44,7 @@ export default function UserSection({
             alt={imagenes.inicio.user.alt}
             title={imagenes.inicio.user.title}
             className={`
-              w-7 h-7 object-contain
+              w-7 h-7 object-contain ${iconSizeMap[size]}
               ${enableDarkMode ? "filter dark:invert-[0.6]" : ""}
             `}
           />
