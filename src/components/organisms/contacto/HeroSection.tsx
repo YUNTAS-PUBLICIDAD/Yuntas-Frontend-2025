42a6ef929dad689daf4dfa5690295ed63413d44d
Bryan Mancilla <bmancilla119@gmail.com>
2026-04-15 23:11:22 -0500

    feat: mejorar diseño y estructura para hero de pagina contacto

---
+++ src/components/organisms/contacto/HeroSection.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Heading from "@/components/atoms/Heading";
+import { imagenes } from "@/data/imagenes";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section
+      className="
+        relative w-full h-[40vh] md:h-[50vh]
+        flex items-center justify-start
+        overflow-hidden
+        border-b-4 border-[#98D8DF]
+      "
+      aria-label="Sección Contacto"
+    >
+
+      <div className="absolute inset-0 scale-110 animate-slowZoom">
+        <Image
+          src={imagenes.contacto.hero.src}
+          alt={imagenes.contacto.hero.alt || "Contacto"}
+          fill
+          priority
+          quality={75}
+          sizes="100vw"
+          className="object-cover object-center"
+        />
+      </div>
+
+      <div
+        className="absolute inset-0 z-10"
+        style={{
+          background:
+            "linear-gradient(to bottom, rgba(0,3,30,0.2), rgba(0,3,30,0.5))",
+        }}
+      />
+
+      <div className="absolute inset-0 z-20 w-full h-full flex items-center justify-center">
+        <div
+          className="
+          flex w-full max-w-[1600px] mx-auto
+          flex-col md:flex-row
+          items-center md:items-center
+          gap-6 md:gap-12
+        "
+        >
+          <div
+            className="
+            flex flex-col w-full
+            justify-center
+            items-center
+            text-center
+          "
+          >
+            <Heading
+              level="h1"
+              size="2xl"
+              className="
+                mb-4 leading-tight
+                drop-shadow-[0_2px_2px_#28BEDA]
+                text-5xl sm:text-6xl md:text-7xl
+              "
+            >
+              CONTACTO
+            </Heading>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
