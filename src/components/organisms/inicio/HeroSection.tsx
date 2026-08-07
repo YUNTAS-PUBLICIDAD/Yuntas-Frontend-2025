86361a568998e78d3a22163184a1d017e599cc5d
Fernanda-cy <fernandavivancoarone@email.com>
2026-08-07 14:01:43 -0500

    fix: corregir URL de pagina de inicio

--- src/components/organisms/inicio/HeroSection.tsx
+++ src/components/organisms/inicio/HeroSection.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/atoms/Button";
 // import Heading from "@/components/atoms/Heading";
 // import Text from "@/components/atoms/Text";
+import { ROUTES } from "@/config/routes";
 import { imagenes } from "@/data/imagenes";
 import { ArrowRight, FileText, Headset, Mail, MapPinned, MessageCircle, Paintbrush, PencilRuler, ShieldCheck, Wrench } from "lucide-react";
 import Image from "next/image";
@@ -114,7 +115,7 @@ const HeroSection = () => {
             <Button
               variant="primary"
               size="lg"
-              href="/contacto"
+              href={ROUTES.CONTACTO}
               icon={<FileText className="h-4 w-4 opacity-80" />}
               className="
                 relative z-20
@@ -148,7 +149,7 @@ const HeroSection = () => {
             </Button>
 
             <Link
-              href={`https://wa.me/51912849782?text=${whatsappMsg}`}
+              href={`${ROUTES.WHATSAPP.YUNTAS}text=${whatsappMsg}`}
               target="_blank"
               rel="noopener noreferrer"
               className="
---
+++ src/config/routes.ts
@@ -0,0 +1,27 @@
+export const ROUTES = {
+
+  CONTACTO: "/contacto",
+
+  PRODUCTOS: {
+    INDEX: "/productos",
+    BARRA_PIXEL_LED: "/productos/barra-pixel-led",
+    LETRERO_NEON_LED: "/productos/letrero-neon-led",
+
+    DETAIL: (slug: string) => `/productos/${slug}`,
+  },
+
+  WHATSAPP: {
+    YUNTAS: "https://wa.me/51912849782",
+  },
+
+  GOOGLE_MAPS: {
+  PROFILE: "https://www.google.com/maps/place/Yuntas+Producciones/@-12.0256654,-76.9420072,17z/data=!4m18!1m9!3m8!1s0x9105c97c8934a213:0x7f6ccb249e86b5e6!2sYuntas+Producciones!8m2!3d-12.0256654!4d-76.9420072!9m1!1b1!16s%2Fg%2F11gzs412yn!3m7!1s0x9105c97c8934a213:0x7f6ccb249e86b5e6!8m2!3d-12.0256654!4d-76.9420072!9m1!1b1!16s%2Fg%2F11gzs412yn?entry=ttu&g_ep=EgoyMDI2MDcwOC4wIKXMDSoASAFQAw%3D%3D",
+  REVIEWS: {
+     GABO:"https://maps.app.goo.gl/GrNEvVoMdYw2Mv4V8", 
+     ABIGAYL: "https://maps.app.goo.gl/hiCx36aBAqAAnhGM7",
+     LUIS:"https://maps.app.goo.gl/oscBifu3hxTfA1xT8",
+  }
+
+}
+
+} as const;
