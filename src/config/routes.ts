70fb5baa1ee116593f7ac22b5c8210a7edfc29ec
Fernanda-cy <fernandavivancoarone@email.com>
2026-09-19 09:48:57 -0500

    fix : Corregir  la variable de redes en configuracion

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -14,13 +14,6 @@ export const ROUTES = {
     YUNTAS: "https://wa.me/51912849782",
   },
 
-  SOCIAL: {
-    INSTAGRAM: "https://www.instagram.com/yuntaspublicidad/",
-    FACEBOOK: "https://www.facebook.com/YuntasProducciones/",
-    TIKTOK: "https://www.tiktok.com/@yuntaspublicidad",
-    YOUTUBE: "https://www.youtube.com/@yuntaspublicidad",
-  },
-
   GOOGLE_MAPS: {
   PROFILE: "https://www.google.com/maps/place/Yuntas+Producciones/@-12.0256654,-76.9420072,17z/data=!4m18!1m9!3m8!1s0x9105c97c8934a213:0x7f6ccb249e86b5e6!2sYuntas+Producciones!8m2!3d-12.0256654!4d-76.9420072!9m1!1b1!16s%2Fg%2F11gzs412yn!3m7!1s0x9105c97c8934a213:0x7f6ccb249e86b5e6!8m2!3d-12.0256654!4d-76.9420072!9m1!1b1!16s%2Fg%2F11gzs412yn?entry=ttu&g_ep=EgoyMDI2MDcwOC4wIKXMDSoASAFQAw%3D%3D",
   REVIEWS: {
